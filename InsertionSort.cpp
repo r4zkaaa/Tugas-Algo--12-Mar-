b1fd99c316f1505164a34517bd54fc30b3af4951
r4zkaaa <rzkiiahmd11@gmail.com>
2026-03-12 13:48:36 +0800

    Six

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -45,4 +45,12 @@ void insertionsort()
         {
             arr[j + 1] = arr[j];
             j--;
-            
+         }
+        arr[j + 1] = temp;
+        cout << "\nstep" << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+}
